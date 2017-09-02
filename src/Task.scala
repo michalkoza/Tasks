a9b3c3fb1295e56fdfbe08f2dca9965fd9659ac5
Michal Koza <michal.koza@microscopeit.com>
2017-09-02 17:20:06 +0200

    cosmetics

--- src/Task.scala
+++ src/Task.scala
@@ -1,10 +1,10 @@
 
-abstract class Task {
+trait Task {
   val configParser : JSONConfigParser
   type Config = configParser.Config
   def run(jsonString: String): Unit = {
     val cfg = configParser.parse(jsonString)
-    execute(cfg)
+    //execute(cfg)
   }
 
   def execute(cfg: Config): Unit
