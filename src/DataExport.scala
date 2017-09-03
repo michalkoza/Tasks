886a6ed420e5a5e4d6e1ebed0b2433b1342e03cb
Tomasz Strumiński <tomasz.struminski@bright-it.com>
2017-09-03 12:21:14 +0200

    Pass more specific parameter to  methods

--- src/DataExport.scala
+++ src/DataExport.scala
@@ -1,20 +1,20 @@
 object DataExport extends Task {
+  case class Blob(account: String, key: String, container: String, path: String)
+  case class ConfigExport(id: String = "DataExportConfig", blob: Blob = Blob("a", "a", "a", "a"))
 
   val configParser = new JSONConfigParser {
+    type Config = ConfigExport
 
-    case class Blob(account: String, key: String, container: String, path: String)
-    case class Config(id: String = "DataExportConfig", blob: Blob = new Blob("a", "a", "a", "a"))
-
-    override def parse(jsonString: String): Config = {
-      println("Parsing " + jsonString)
-      val cfg = new Config()
-      println(cfg.toString())
+    override def parse(jsonString: String): ConfigExport = {
+      println(s"Parsing $jsonString")
+      val cfg = ConfigExport()
+      println(cfg.toString)
       cfg
     }
   }
 
-  override def execute(cfg: Config): Unit = {
+  override def execute(cfg: ConfigExport): Unit = {
     println("Doing DataExportConfig proprietary things")
-    //println("Config ID = "+cfg.id)
+    println(s"Config ID = ${cfg.id}")
   }
 }
--- src/UploadExtractor.scala
+++ src/UploadExtractor.scala
@@ -1,20 +1,20 @@
 object UploadExtractor extends Task {
+  case class Blob(account: String, key: String, container: String, path: String)
+  case class ConfigUpload(id: String = "UploadExtractionConfig", blob: Blob = Blob("a", "a", "a", "a"), info: String = "emptyinfo")
 
   val configParser = new JSONConfigParser {
+    type Config = ConfigUpload
 
-    case class Blob(account: String, key: String, container: String, path: String)
-    case class Config(id: String = "UploadExtractionConfig", blob: Blob = new Blob("a", "a", "a", "a"), info: String = "emptyinfo")
-
-    override def parse(jsonString: String): Config = {
-      println("Parsing " + jsonString)
-      val cfg = new Config()
-      println(cfg.toString())
+    override def parse(jsonString: String) = {
+      println(s"Parsing $jsonString")
+      val cfg = ConfigUpload()
+      println(cfg.toString)
       cfg
     }
   }
 
-  override def execute(cfg: Config): Unit = {
+  override def execute(cfg: ConfigUpload): Unit = {
     println("Doing UploadExtraction proprietary things")
-    //println("Config ID = "+cfg.)
+    println(s"Config ID = ${cfg.id}")
   }
 }
